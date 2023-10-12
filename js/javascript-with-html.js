4155fc343a25af41607494c66fbb07e50b1d8dba
Corey Harmon <harmon.coreyd@gmail.com>
2023-10-12 16:14:48 -0500

    java script to html lecture

---
+++ js/javascript-with-html.js
@@ -0,0 +1,17 @@
+"use strict"
+
+let myAge = 5;
+console.log(myAge)
+
+
+alert("Hello Lasso")
+// How to interact with the user (a popup on the browser)
+
+let confirmed = confirm("Are you sure you're in the correct cohort?")
+console.log(confirmed)
+
+console.log("This is my external js file")
+
+//prompt - where you can type in your name or password
+let userInput = prompt("Please type in your name")
+console.log(`Great to meet ${userInput}`)
