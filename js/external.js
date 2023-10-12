1f2677798b95937cdf7ffed7cb45c22384b44fcc
Corey Harmon <harmon.coreyd@gmail.com>
2023-10-12 16:52:47 -0500

    js to html exercise

---
+++ js/external.js
@@ -0,0 +1,7 @@
+console.log("Hello from external change")
+
+alert("Welcome to my Website!")
+
+let userInput = prompt("What is your favorite color?")
+
+let confirmed = confirm(`Great ${userInput} is my favorite color too!`)
