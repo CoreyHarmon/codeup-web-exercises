9e3bd091b48c47cfbbfbb94cb7a0bb4bfa837abb
Corey Harmon <harmon.coreyd@gmail.com>
2023-10-24 10:16:04 -0500

    dates and birthdays

--- js/objects_lec.js
+++ js/objects_lec.js
@@ -174,3 +174,53 @@ car.logMakeModel = function () {
 
 // log the make/model
 car.logMakeModel();
+
+
+
+
+
+// Date Objects
+// Creates a JavaScript Date instance that represents a single moment in time. Date objects are based on a time value that is the number of milliseconds since 1 January 1970 UTC.
+
+// Creating a Date
+// Here are some examples of different ways to create a Date object in JavaScript:
+
+'use strict';
+
+// get a date object that represents the current date/time
+let today = new Date();
+
+// create a date object based on a specific date/time
+// notice that month is zero based in non-string constructors
+let codeup = new Date("February 4, 2014 09:00:00");
+codeup = new Date("2014-02-04T09:00:00");
+codeup = new Date(2014, 1, 4);
+codeup = new Date(2014, 1, 4, 9, 0, 0);
+// Formatting Dates
+// The options for formatting dates in JavaScript are somewhat limited. If you want to print out a full date, you can use the toString() method as follows:
+
+    'use strict';
+
+let codeup = new Date(2014, 1, 4, 9, 0, 0);
+codeup.toString(); // Tue Feb 04 2014 09:00:00 GMT-0600 (CST)
+// If you do not want the full date string, you can use other Date methods to retrieve individual portions of the date and build the string yourself.
+//
+//     Calculating Elapsed Time
+// You can calculate elapsed time using the Date.now() function to get a timestamp at the start and end of an activity. The elapsed time will be the difference between the two timestamps.
+//
+//     According to the documentation:
+//
+//     The Date.now() method returns the number of milliseconds elapsed since 1 January 1970 00:00:00 UTC.
+
+'use strict';
+
+// get the start timestamp
+let start = Date.now();
+
+// do something for a long time
+
+// get the end timestamp
+let end = Date.now();
+
+// calculate the elapsed time in milliseconds
+let elapsed = end - start;
