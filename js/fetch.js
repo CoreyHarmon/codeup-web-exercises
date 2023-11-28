c93fd09eb5bc5c5db97707d4ffb12a8d65c035e1
Corey Harmon <harmon.coreyd@gmail.com>
2023-11-28 11:39:24 -0600

    finished fetch.js exercise

--- js/fetch.js
+++ js/fetch.js
@@ -1,17 +1,12 @@
 
-function getLastCommitDate(username) {
+function findLastCommit (name) {
 
+    let url =  `https://api.github.com/users/${name}/events/public`
+    fetch(url,  {headers: {'Authorization': 'token' + GITHUB_API_KEY}})
+        // turning into json to make it more viewable
+        .then(response => response.json())
+        .then(data => console.log(data[0].payload.commits[0]))
+        .catch(error => console.log("Error" + error))
 }
 
-fetch(url, {headers: {'Authorization': 'token' + GITHUB_API_KEY}})
-    .then(response => response.json)
-    .then(data => {
-        const username = '';
-        getLastCommitDate(username)
-            .then(lastCommitDate => {
-                console.log(`The last commit by ${username} was made on ${lastCommitDate}.`);
-            })
-    })
-    .catch(error => {
-        console.error("Error fetching data:", error);
-    });
+findLastCommit("xJohnnyMartinezx") // type in any user-name to fill in above url
