afea307bffbf8516e043cf6453c59afb08a63232
Corey Harmon <harmon.coreyd@gmail.com>
2023-11-30 12:30:12 -0600

    styling mostly done for weather project

---
+++ js/weather_map.js
@@ -0,0 +1,163 @@
+"use strict";
+
+/***
+ * geocode is a method to search for coordinates based on a physical address and return
+ * @param {string} search is the address to search for the geocoded coordinates
+ * @param {string} token is your API token for MapBox
+ * @returns {Promise} a promise containing the latitude and longitude as a two element array
+ *
+ * EXAMPLE:
+ *
+ *  geocode("San Antonio", API_TOKEN_HERE).then(function(results) {
+ *      // do something with results
+ *  })
+ *
+ */
+function geocode(search, token) {
+    // api url
+    let baseUrl = 'https://api.mapbox.com';
+    // endpoint when I request long and lat based on location name
+    let endPoint = '/geocoding/v5/mapbox.places/';
+    // fetch request that takes in a url endpoint that will return the long/lat
+    return fetch(`${baseUrl}${endPoint}${encodeURIComponent(search)}.json?access_token=${token}`)
+        .then( res => res.json() )
+        // to get all the data from the request, comment out the following three lines...
+        .then( data => data.features[0].center);
+}
+
+
+
+/***
+ * reverseGeocode is a method to search for a physical address based on inputted coordinates
+ * @param {object} coordinates is an object with properties "lat" and "lng" for latitude and longitude
+ * @param {string} token is your API token for MapBox
+ * @returns {Promise} a promise containing the string of the closest matching location to the coordinates provided
+ *
+ * EXAMPLE:
+ *
+ *  reverseGeocode({lat: 32.77, lng: -96.79}, API_TOKEN_HERE).then(function(results) {
+ *      // do something with results
+ *  })
+ *
+ */
+function reverseGeocode(coordinates, token) {
+    let baseUrl = 'https://api.mapbox.com';
+    let endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(`${baseUrl}${endPoint}${coordinates.lng},${coordinates.lat}.json?access_token=${token}`)
+        .then( res => res.json() )
+        // to get all the data from the request, comment out the following three lines...
+        .then( data => data.features[0].place_name );
+}
+
+
+
+// MapBox info
+const ACCESS_TOKEN = MAPBOX_API;
+
+ mapboxgl.accessToken = ACCESS_TOKEN;
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    zoom: 10,
+    center: [-96.7970, 32.7767]
+});
+
+// Location of Oak'd BBQ - Marker
+const marker = new mapboxgl.Marker()
+    .setLngLat([-96.76863197472666, 32.85616710706724])
+    .addTo(map);
+
+// Pop-up with name of Restaurant
+const codeupPopup = new mapboxgl.Popup()
+    .setHTML("<p>Oak'd BBQ</p>");
+
+// Pops up when clicked
+marker.setPopup(codeupPopup);
+
+
+<!--number 8, create an array, and use a forEach-->
+const restaurants = [
+    {
+        name: "Oak'd BBQ",
+        coordinates: [-96.76863197472666, 32.85616710706724],
+        description: "Delicious BBQ in Dallas, TX"
+    },
+    {
+        name: "BJ's Restaurant & Brewhouse",
+        coordinates: [-96.8263108568239, 32.96039931854623],
+        description: "Authentic Italian pizza in Dallas, TX"
+    },
+    {
+        name: "Blue Fish",
+        coordinates: [-96.773192939669, 32.85096197505924],
+        description: "Fresh and tasty sushi in Dallas, TX"
+    }
+];
+
+restaurants.forEach(restaurant => {
+    const marker = new mapboxgl.Marker()
+        .setLngLat(restaurant.coordinates)
+        .addTo(map);
+
+    const popup = new mapboxgl.Popup()
+        .setHTML(`<p>${restaurant.name}</p><p>${restaurant.description}</p>`);
+
+    marker.setPopup(popup);
+});
+
+// Search Functionality
+document.getElementById("sub").addEventListener("click", function() {
+    // Grab the value that the user enters below. .value is whatever value they enter the search box
+    let currentLocation = geocode(document.getElementById("search").value, MAPBOX_API);
+
+    currentLocation.then(result => {
+        console.log(result)
+        map.setCenter([result[0], result[1]])
+    })
+})
+
+// Making it show only 5 days in the forecast
+const weatherOutput = document.querySelector("#forecast")
+
+fetch(`https://api.openweathermap.org/data/2.5/forecast?` +
+    'id=4726206' +
+    `&appid=${MAPBOX_API}` + `&units=imperial`)
+    .then(data => data.json())
+    .then(result => {
+        console.log(result)
+
+        // result.list.forEach(weather => {
+        //     const date = new Date(weather.dt * 1000);
+        //     console.log(date.toLocaleDateString());
+        //
+        //     const time = document.createElement("p")
+        //     const temp = document.createElement("p")
+        //
+        //     time.innerText = date;
+        //     temp.innerText = weather.main.temp;
+        //
+        //     weatherOutput.appendChild(time)
+        //     weatherOutput.appendChild(temp)
+        //
+        // })
+
+        const day = result.list;
+
+        for (let i = 0; i < day.length; i += 8) {
+            const weather = day[i]
+
+            const date = new Date(weather.dt * 1000);
+            console.log(date.toLocaleDateString());
+
+            const time = document.createElement("p")
+            const temp = document.createElement("p")
+
+            time.innerText = date;
+            temp.innerText = weather.main.temp;
+
+            weatherOutput.appendChild(time)
+            weatherOutput.appendChild(temp)
+        }
+
+    })
